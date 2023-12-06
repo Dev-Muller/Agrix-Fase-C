f33c3148f7d3d585106285c5afe34b52e0bab5ca
muller <pedromullergl11@gmail.com>
2023-12-06 19:02:11 -0300

    fix: fix checkstyles

--- src/main/java/com/betrybe/agrix/advice/GeneralControllerAdvice.java
+++ src/main/java/com/betrybe/agrix/advice/GeneralControllerAdvice.java
@@ -4,7 +4,6 @@ import com.betrybe.agrix.exception.CropNotFoundException;
 import com.betrybe.agrix.exception.FarmNotFoundException;
 import com.betrybe.agrix.exception.FertilizerNotFoundException;
 import com.betrybe.agrix.exception.PersonNotFoundException;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
